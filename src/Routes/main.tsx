fc719bfb95770c1f7b1e5fc1aea3c4c012d6f2dc
bailey-redbrick <bailey@redbrick.space>
2023-01-31 11:44:16 +0900

    Modify: display minted nft image

--- src/Routes/main.tsx
+++ src/Routes/main.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useState } from "react";
 import { mintAnimalTokenContract } from "../contracts";
+import AnimalCard from "../components/AnimalCard";
 
 interface MainProps {
   account: string;
 }
 
 const Main: FC<MainProps> = ({ account }) => {
-  const [newAnimalCard, setNewAnimalCard] = useState<string>();
+  const [newAnimalType, setNewAnimalType] = useState<string>();
 
   const onClickMint = async () => {
     console.log(account);
@@ -19,13 +20,20 @@ const Main: FC<MainProps> = ({ account }) => {
         const balanceLength = await mintAnimalTokenContract.methods.balanceOf(account).call();
         const animalTokenId = await mintAnimalTokenContract.methods.tokenOfOwnerByIndex(account, parseInt(balanceLength.length, 10) - 1).call();
         const animalType = await mintAnimalTokenContract.methods.animalTypes(animalTokenId).call();
-        setNewAnimalCard(animalType);
+        setNewAnimalType(animalType);
+        console.log(balanceLength, animalTokenId, animalType);
       }
     } catch (err) {
       console.error(err);
     }
   };
-  return <div>{!newAnimalCard && <button onClick={onClickMint}>Mint animal card</button>}</div>;
+
+  return (
+    <div>
+      {!newAnimalType && <button onClick={onClickMint}>Mint animal card</button>}
+      <AnimalCard animalType={newAnimalType || "default"} />
+    </div>
+  );
 };
 
 export default Main;
